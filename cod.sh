54d5e40f2c03939185e72f7409925b69826a9987
Vinicius Gustavo <viniciusgustavodejesus@gmail.com>
2018-11-01 20:20:49 -0300

    Delete cod.sh

--- cod.sh
+++
@@ -1,9 +0,0 @@
-sed -i '/##/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/--/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/Sensor/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/temperature/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/absolute/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/relative/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/computed/d' 29-05-2018---16-08.txt > teste.txt
-sed -i '/provided/d' 29-05-2018---16-08.txt > teste.txt
-
